93ca6cfd2e1a377ebe494e7aa8faa53554d072c9
RazRamon <razramon123@gmail.com>
2017-03-30 18:24:16 +0300

    commit change

---
+++ submarinesGame/ISubmarinesGameAlgo.cpp
@@ -0,0 +1,28 @@
+#include "ISubmarinesGameAlgo.h"
+
+
+class ISubmarinesGameAlgo {
+public:
+	void setBoard(const char** board, int numRows, int numCols) 
+	{
+
+
+	}
+
+	std::pair<int, int> ISubmarinesGameAlgo::attack()
+	{
+
+
+
+		return std::pair<int, int>();
+	}
+
+	void notifyOnAttackResult(int player, int row, int col, AttackResult result) 
+	{
+
+
+
+	}
+
+
+};
