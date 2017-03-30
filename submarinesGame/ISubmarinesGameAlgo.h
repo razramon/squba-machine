3dfaa85cd67cf0ae7334dc0c30f0de7102fb34b2
RazRamon <razramon123@gmail.com>
2017-03-30 18:39:38 +0300

    commit changes to header

--- submarinesGame/ISubmarinesGameAlgo.h
+++ submarinesGame/ISubmarinesGameAlgo.h
@@ -1,14 +1,15 @@
 #pragma once
+
 #include <utility> // for std::pair
 
 enum class AttackResult {
 	Miss, Hit, Sink
 };
 
-class ISubmarinesGameAlgo {
+class IBattleshipGameAlgo {
 public:
-	virtual ~ISubmarinesGameAlgo() = default;
-	void setBoard(const char** board, int numRows, int numCols); // called once to notify player on his board
-	std::pair<int, int> attack(); // ask player for his move
-	void notifyOnAttackResult(int player, int row, int col, AttackResult result); // notify on last move result
+	virtual ~IBattleshipGameAlgo() = default;
+	virtual void setBoard(const char** board, int numRows, int numCols) = 0; // called once to notify player on his board
+	virtual std::pair<int, int> attack() = 0; // ask player for his move
+	virtual void notifyOnAttackResult(int player, int row, int col, AttackResult result) = 0; // notify on last move result
 };
