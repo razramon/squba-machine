5d3996bd632b2281e8f038cd33ca33888387e3c2
RazRamon <razramon123@gmail.com>
2017-04-03 20:50:53 +0300

    start check boardon maind

--- submarinesGame/submarinesGame.cpp
+++ submarinesGame/submarinesGame.cpp
@@ -1,4 +1,6 @@
 #include <windows.h>
+#include "Ship.h"
+#include "Sizes.h"
 
 //returns true if the path exists
 bool doesPathExist(const char* path)
@@ -26,3 +28,37 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+bool checkBoard(char ** board)
+{
+	Ship shipA[NUMBER_SHIPS];
+	Ship shipB[NUMBER_SHIPS];
+	int indexShipA = 0;
+	int indexShipB = 0;
+	int numberOfShipsA = 0;
+	int numberOfShipsB = 0;
+
+	for(int indexRow = 0; indexRow < BOARD_LENGTH - 1; indexRow++)
+	{
+		for(int indexColumn = 0; indexColumn < BOARD_LENGTH -1; indexColumn++)
+		{
+			if(islower(board[indexRow][indexColumn]) && isLetterOfShip(board[indexRow][indexColumn]))
+			{
+
+
+			}
+
+
+
+		}
+
+	}
+	//islower
+	return true;
+}
+bool isLetterOfShip(char c)
+{
+	c = tolower(c);
+	if (c == 'b' || c == 'p' || c == 'm' || c == 'd')
+		return true;
+	return false;
+}
