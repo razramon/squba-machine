7b7f627df04fa7e0e003d463dde092436f01843d
RazRamon <razramon123@gmail.com>
2017-04-03 19:32:31 +0300

    changes to: ship, player adding header sizes, macros

--- submarinesGame/Player.cpp
+++ submarinesGame/Player.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include "Ship.h"
+#include "Sizes.h"
 using namespace std;
 
 class Player {
 	Ship ships[5];
-	char playerLetter;
+	int playerNum;
 	int totalNumberOfPoints;
-	char ** board;
 
+public:
+	void printBoard()
+	{
+		for(int indexRow = 0; indexRow < BOARD_LENGTH; indexRow++)
+		{
+			for(int indexColumn = 0; indexColumn < BOARD_LENGTH; indexColumn++)
+			{
+				for(int indexShip = 0; indexShip < NUMBER_SHIPS; indexShip++)
+				{
+
+				}
+
+
+			}
+
+
+		}
+
+	}
 
 };
