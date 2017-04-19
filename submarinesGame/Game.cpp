aa7766187149caacde9e4c38e6f3bc67add922a6
RazRamon <razramon123@gmail.com>
2017-04-20 01:07:50 +0300

    push changes to game, game.h

--- submarinesGame/Game.cpp
+++ submarinesGame/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
-
+#include "Ship.h"
+#include "Sizes.h"
 
 
 Game::Game()
@@ -11,65 +12,69 @@ Game::~Game()
 {
 }
 
-//void Player::setBoard(const char** board, int numRows, int numCols)
-//{
-//	// Looping over all the ships of the player
-//	for (int indexShip = 0; indexShip < NUMBER_SHIPS; indexShip++)
-//	{
-//		// Looping over the position of each ship
-//		for (int pos = 0; pos < Ship::sizeOfShip(ships[indexShip].getLetter()); pos++)
-//		{
-//			int row = ships[indexShip].getPosition()[pos][0];
-//			int col = ships[indexShip].getPosition()[pos][1];
-//			ships[indexShip].setPosition(pos, row, col, board[row][col]);
-//		}
-//	}
-//}
-//int Player::isHit(int row, int col)
-//{
-//	int numberOfHitInShip = 0;
-//	char letterHit = ' ';
-//	int indexHitShip = 0;
-//	bool hit = false;
-//	int hitBefore = 0;
-//
-//	// Looping over all the ships of the player
-//	for (int indexShip = 0; indexShip < NUMBER_SHIPS; indexShip++)
-//	{
-//		numberOfHitInShip = 0;
-//		// Looping over the position of each ship
-//		for (int pos = 0; pos < Ship::sizeOfShip(ships[indexShip].getLetter()); pos++)
-//		{
-//			// If there is a match, return true - there is a hit
-//			if (ships[indexShip].getPosition()[pos][0] == row && ships[indexShip].getPosition()[pos][1] == col)
-//			{
-//				hitBefore = ships[indexShip].getPosition()[pos][2];
-//				hit = true;
-//				indexHitShip = indexShip;
-//				letterHit = ships[indexShip].getLetter();
-//			}
-//
-//			numberOfHitInShip += ships[indexShip].getPosition()[pos][2];
-//		}
-//		if (hit)
-//			break;
-//	}
-//
-//	// If was hit
-//	if (hitBefore != 0)
-//		return 3;
-//	// If there is a hit and all the ship was hit - sink
-//	else if (hit && numberOfHitInShip == Ship::sizeOfShip(letterHit) - 1 && !ships[indexHitShip].isSunk())
-//		return 2;
-//	// Check if there was atleast a hit
-//	else if (hit)
-//		return 1;
-//	// Miss
-//	else
-//		return 0;
-//}
-//void Player::notifyOnAttackResult(int player, int row, int col, AttackResult result)
-//{
+void Game::setBoard(const char** board, int numRows, int numCols)
+{
+	for (int i = 0; i < 2; i++) {
+		// Looping over all the ships of the player
+		for (int indexShip = 0; indexShip < NUMBER_SHIPS; indexShip++)
+		{
+			// Looping over the position of each ship
+			for (int pos = 0; pos < Ship::sizeOfShip(ships[i][indexShip].getLetter()); pos++)
+			{
+				int row = ships[i][indexShip].getPosition()[pos][0];
+				int col = ships[i][indexShip].getPosition()[pos][1];
+				ships[i][indexShip].setPosition(pos, row, col, board[row][col]);
+			}
+		}
+	}
+}
+int Game::isHit(int row, int col)
+{
+	Ship ships[5];
+	int numberOfHitInShip = 0;
+	char letterHit = ' ';
+	int indexHitShip = 0;
+	bool hit = false;
+	int hitBefore = 0;
+
+	// Looping over all the ships of the player
+	for (int indexShip = 0; indexShip < NUMBER_SHIPS; indexShip++)
+	{
+		numberOfHitInShip = 0;
+		// Looping over the position of each ship
+		for (int pos = 0; pos < Ship::sizeOfShip(ships[indexShip].getLetter()); pos++)
+		{
+			// If there is a match, return true - there is a hit
+			if (ships[indexShip].getPosition()[pos][0] == row && ships[indexShip].getPosition()[pos][1] == col)
+			{
+				hitBefore = ships[indexShip].getPosition()[pos][2];
+				hit = true;
+				indexHitShip = indexShip;
+				letterHit = ships[indexShip].getLetter();
+			}
+
+			numberOfHitInShip += ships[indexShip].getPosition()[pos][2];
+		}
+		if (hit)
+			break;
+	}
+
+	// If was hit
+	if (hitBefore != 0)
+		return 3;
+	// If there is a hit and all the ship was hit - sink
+	else if (hit && numberOfHitInShip == Ship::sizeOfShip(letterHit) - 1 && !ships[indexHitShip].isSunk())
+		return 2;
+	// Check if there was atleast a hit
+	else if (hit)
+		return 1;
+	// Miss
+	else
+		return 0;
+}
+void Game::notifyOnAttackResult(int player, int row, int col, AttackResult result)
+{
+}
 //	switch (result)
 //	{
 //	case AttackResult::Miss:
@@ -100,14 +105,14 @@ Game::~Game()
 //		}
 //	}
 //}
-//std::pair<int, int> Player::attack()
-//{
-//	pair<int, int> attack = attacks[++attackNumber];
-//
-//	// Checking if the number of the attack equal the size of the vector, if so changing to -1 to say there aren't more moves
-//	if (attackNumber == attacks.size() - 1)
-//	{
-//		attackNumber = -1;
-//	}
-//	return attack;
-//}
+std::pair<int, int> Game::attack()
+{
+	//std::pair<int, int> attack = attacks[++attackNumber];
+
+	//// Checking if the number of the attack equal the size of the vector, if so changing to -1 to say there aren't more moves
+	//if (attackNumber == attacks.size() - 1)
+	//{
+	//	attackNumber = -1;
+	//}
+	//return attack;
+}
--- submarinesGame/Game.h
+++ submarinesGame/Game.h
@@ -1,11 +1,24 @@
 #pragma once
 
 #include "IBattleshipGameAlgo.h"
+#include "Ship.h"
+#include <vector>
 
 class Game : public IBattleshipGameAlgo
 {
 public:
 	Game();
 	~Game();
+
+	void setBoard(const char ** board, int numRows, int numCols) override;
+
+	int isHit(int row, int col);
+
+	void notifyOnAttackResult(int player, int row, int col, AttackResult result) override;
+
+	std::pair<int, int> attack() override;
+
+private:
+	std::vector<Ship> ships[2];
 };
 
