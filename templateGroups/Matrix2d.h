6d47dc7a37eef2e1668632fde1571dbb8ad8d731
RazRamon <razramon123@gmail.com>
2017-06-26 19:41:38 +0300

    changes

--- templateGroups/Matrix2d.h
+++ templateGroups/Matrix2d.h
@@ -1,7 +1,15 @@
 #pragma once
 
 
-class Matrix2d {
+template<class T, size_t DIMENSIONS>
+class Matrix {
 
 
 };
+
+
+template<class T>
+using Matrix2d = Matrix<T, 2>;
+
+template<class T>
+using Matrix3d = Matrix<T, 3>;
